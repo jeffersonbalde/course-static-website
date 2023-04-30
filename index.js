f86f8133d123aa1bb00d5cf1e085bee0a61b1ab8
Jefferson Balde <jeffersonbalde8@gmail.com>
2023-04-30 23:18:37 +0800

    navbar animate

--- index.js
+++ index.js
@@ -3,3 +3,10 @@
 // form.addEventListener('submit', (e) => {
 //     e.preventDefault()
 // })
+
+const nav = document.querySelector("nav");
+
+window.addEventListener("scroll" , () => {
+    if(window.pageYOffset > 60) nav.classList.add("scrolled");
+    else nav.classList.remove("scrolled");
+})
